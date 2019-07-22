3237e90e2f7271be694b5cc23d2f15f6b862dff5
sentriz <senan@senan.xyz>
2019-07-22 16:52:41 +0100

    fix background on mobile

--- tmpl.go
+++ tmpl.go
@@ -6,30 +6,31 @@ var homeTmpl = `<!doctype html>
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <title>{{ .PageTitle }}</title>
   <style>
+  :root {
+    --main-pad-size: 1.2rem;
+    --main-width: 600px;
+  }
   * {
     margin: 0;
     padding: 0;
   }
   body {
-    max-width: 600px;
+    max-width: var(--main-width);
     margin: 0 auto;
-    overflow-y: hidden;
     font-family: monospace;
   }
   #container {
-    height: 100vh;
-    position: relative;
-    padding: 1.2rem;
+    padding: var(--main-pad-size);
   }
-  div::after {
+  #container::after {
     content: "";
     background-image: url("https://www.toptal.com/designers/subtlepatterns/patterns/full-bloom.png");
     opacity: 0.6;
     top: 0;
-    left: 0;
     bottom: 0;
-    right: 0;
-    position: absolute;
+    width: var(--main-width);
+    transform: translateX(calc(var(--main-pad-size) * -1));
+    position: fixed;
     z-index: -1;
   }
   hr {
@@ -61,6 +62,81 @@ var homeTmpl = `<!doctype html>
     <strong>{{ .PageTitle }}</strong>
     <br>
     <br>
+    {{ range $project, $containers := .Projects }}
+    <hr>
+    <p><strong>{{ $project }}</strong></p>
+    <table class="c-stats">
+    {{ range $container := $containers }}
+      {{ $isDown := eq $container.Status "" }}
+      {{ if $isDown }}
+        <tr class="red">
+      {{ else }}
+        <tr class="green">
+      {{ end }}
+      {{ if not (eq $container.Link "") }}
+        <td><a href="//{{ $container.Link }}" target="_blank">{{ $container.ID }}</a></td>
+      {{ else }}
+        <td>{{ $container.ID }}</td>
+      {{ end }}
+      {{ if $isDown }}
+        <td>last seen {{ $container.LastSeen | humanDate }}</td>
+      {{ else }}
+        <td>{{ $container.Status }}</td>
+      {{ end }}
+      </tr>
+    {{ end }}
+    </table>
+    {{ end }}
+    {{ range $project, $containers := .Projects }}
+    <hr>
+    <p><strong>{{ $project }}</strong></p>
+    <table class="c-stats">
+    {{ range $container := $containers }}
+      {{ $isDown := eq $container.Status "" }}
+      {{ if $isDown }}
+        <tr class="red">
+      {{ else }}
+        <tr class="green">
+      {{ end }}
+      {{ if not (eq $container.Link "") }}
+        <td><a href="//{{ $container.Link }}" target="_blank">{{ $container.ID }}</a></td>
+      {{ else }}
+        <td>{{ $container.ID }}</td>
+      {{ end }}
+      {{ if $isDown }}
+        <td>last seen {{ $container.LastSeen | humanDate }}</td>
+      {{ else }}
+        <td>{{ $container.Status }}</td>
+      {{ end }}
+      </tr>
+    {{ end }}
+    </table>
+    {{ end }}
+    {{ range $project, $containers := .Projects }}
+    <hr>
+    <p><strong>{{ $project }}</strong></p>
+    <table class="c-stats">
+    {{ range $container := $containers }}
+      {{ $isDown := eq $container.Status "" }}
+      {{ if $isDown }}
+        <tr class="red">
+      {{ else }}
+        <tr class="green">
+      {{ end }}
+      {{ if not (eq $container.Link "") }}
+        <td><a href="//{{ $container.Link }}" target="_blank">{{ $container.ID }}</a></td>
+      {{ else }}
+        <td>{{ $container.ID }}</td>
+      {{ end }}
+      {{ if $isDown }}
+        <td>last seen {{ $container.LastSeen | humanDate }}</td>
+      {{ else }}
+        <td>{{ $container.Status }}</td>
+      {{ end }}
+      </tr>
+    {{ end }}
+    </table>
+    {{ end }}
     {{ range $project, $containers := .Projects }}
     <hr>
     <p><strong>{{ $project }}</strong></p>
