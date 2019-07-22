d07e383a2ca8dc67a10f88e80fe277bfb4a53c46
sentriz <senan@senan.xyz>
2019-07-22 16:25:42 +0100

    add markup metadata

--- tmpl.go
+++ tmpl.go
@@ -3,6 +3,8 @@ package main
 var homeTmpl = `<!doctype html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <title>{{ .PageTitle }}</title>
   <style>
   * {
     margin: 0;
